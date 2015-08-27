93934e9177835368c5af610b7178c09727f570c5
lisaiguang <lisaiguang@gmail.com>
2015-08-27 21:37:03 +0800

    add

--- public/poker/main.js
+++ public/poker/main.js
@@ -2,5 +2,4 @@
  * Created by gapp on 8/27/15.
  */
 define(function(require){
-
 })
--- public/poker/rules.js
+++ public/poker/rules.js
@@ -2,6 +2,9 @@
  * Created by gapp on 8/27/15.
  */
 define(function(){
+    var list = [
+        [1,1,1,1,1]
+    ];
     return {
         compare:function(l1,l2){
         }
