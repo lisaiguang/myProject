50a02874685ed8d47b9d61b6c5b0e2e399c0ae6e
HFY <lisaiguang@qq.com>
2015-08-28 12:47:26 +0800

    add

--- public/poker/hands.js
+++ public/poker/hands.js
@@ -3,10 +3,14 @@
  */
 define(function(){
     var list = [
+        [2],
+        [3,[2]],
         [1,1,1,1,1]
     ];
     return {
         compare:function(l1,l2){
+        },
+        calculate:function(l){
         }
     };
 },module);
