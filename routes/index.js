dc5a6ef8e355b384843cfa3beeb9382d3c7ac355
HFY <lisaiguang@qq.com>
2015-09-08 08:49:34 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/test', function(req, res, next) {
     res.render('index', {  main:'poker' });
 });
+
+router.get('/', function(){
+
+})
 
 module.exports = router;
