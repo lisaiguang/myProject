0d276eae8968ca2bcf98a455d81e881362ca48d9
lisaiguang <lisaiguang@gmail.com>
2015-09-09 22:38:39 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('index', {  main:'poker' });
 });
-
-router.get('/', function(){
-
-})
 
 module.exports = router;
